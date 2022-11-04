70bee13fcc371627ee88db37c69d24ff5600be1e
Kamtheman56 <misterman3@outlook.com>
2022-11-04 15:24:37 -0400

    Massive updates

--- src/main/java/com/kamth/zeldamod/item/custom/HeavyBoots.java
+++ src/main/java/com/kamth/zeldamod/item/custom/HeavyBoots.java
@@ -1,31 +1,1 @@
-package com.kamth.zeldamod.item.custom;
-
-
-import com.kamth.zeldamod.item.ModItems;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.entity.EquipmentSlot;
-
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ArmorItem;
-import net.minecraft.world.item.ArmorMaterial;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-public class HeavyBoots extends ArmorItem {
-    public HeavyBoots(ArmorMaterial p_40386_, EquipmentSlot p_40387_, Properties p_40388_) {
-        super(p_40386_, p_40387_, p_40388_);
-    }
-
-    @SubscribeEvent
-    public static void onPlayerTick(Player player) {
-        boolean isWearingHeavyBoots = true;
-        if (player.getItemBySlot(EquipmentSlot.FEET).getItem() == ModItems.HEAVY_BOOTS.get())
-            {
-
-                player.flyingSpeed = 0.04F;
-
-
-            }
-
-        }
-
-    }
-
+package com.kamth.zeldamod.item.custom;import com.kamth.zeldamod.item.ModItems;import net.minecraft.world.entity.EquipmentSlot;import net.minecraft.world.entity.player.Player;import net.minecraft.world.item.ArmorItem;import net.minecraft.world.item.ArmorMaterial;import net.minecraftforge.eventbus.api.SubscribeEvent;public class HeavyBoots extends ArmorItem {    public HeavyBoots(ArmorMaterial p_40386_, EquipmentSlot p_40387_, Properties p_40388_) {        super(p_40386_, p_40387_, p_40388_);    }    @SubscribeEvent    public static void onLivingTickEvent (Player player) {        if (player.getItemBySlot(EquipmentSlot.FEET).getItem() == ModItems.HEAVY_BOOTS.get())            {            }        }    }
