89a30234bb692aa904d03b23244bc510dc4270bb
Kamtheman56 <misterman3@outlook.com>
2024-03-03 11:14:59 -0600

    Working on epic fight compat, Zora swimming started but might need some tweaking still

---
+++ src/main/java/com/kamth/mixin/MixinPlayerModel.java
@@ -0,0 +1,58 @@
+package com.kamth.mixin;
+
+
+import com.kamth.zeldamod.item.ModItems;
+import com.kamth.zeldamod.item.items.GliderItem;
+import com.kamth.zeldamod.item.masks.ZoraMask;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.MouseHandler;
+import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.client.model.PlayerModel;
+import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.util.Mth;
+import net.minecraft.util.SmoothDouble;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.BowItem;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PlayerModel.class)
+public abstract class MixinPlayerModel extends HumanoidModel<LivingEntity> {
+    private static final float ARM_ROTATION = (float)(Math.PI*2-2.9);
+
+    public MixinPlayerModel(ModelPart part){
+        super(part);
+    }
+
+    @Inject(
+            method = "setupAnim(Lnet/minecraft/world/entity/LivingEntity;FFFFF)V",
+            at = {
+                    @At(shift = At.Shift.AFTER, value = "INVOKE", target = "Lnet/minecraft/client/model/HumanoidModel;setupAnim(Lnet/minecraft/world/entity/LivingEntity;FFFFF)V")
+            }
+    )
+    public void onSetRotationAngles(LivingEntity entity,
+                                    float limbSwing,
+                                    float limbSwingAmount,
+                                    float ageInTicks,
+                                    float netHeadYaw,
+                                    float headPitch,
+                                    CallbackInfo ci){
+        ItemStack stack = entity.getItemBySlot(EquipmentSlot.HEAD);
+        if(stack.getItem() instanceof ZoraMask && entity.isSwimming()){
+            leftArm.xRot = ARM_ROTATION;
+            leftArm.zRot = 34.5f;
+            rightArm.xRot = ARM_ROTATION ;
+            rightArm.zRot = -34.5f;
+         //   leftLeg.xRot = 0f;
+         //   rightLeg.xRot = ;
+        }
+    }
+
+
+}
