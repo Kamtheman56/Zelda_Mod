75c1c347c0e93a3e07376367d6fd85579feb9d68
deadlydiamond98 <diamonderrick1@gmail.com>
2025-02-06 17:40:38 -0500

    Fixed Crash with Pressure Switch (Issue #32)

--- src/main/java/com/kamth/zeldamod/block/custom/redstone_related/RustedTimedPressureSwitchBlock.java
+++ src/main/java/com/kamth/zeldamod/block/custom/redstone_related/RustedTimedPressureSwitchBlock.java
@@ -39,39 +39,41 @@ public class RustedTimedPressureSwitchBlock extends Block {
     protected static final VoxelShape FLAT = flat();
     public final int ticksToStayPressed;
     private final boolean arrowsCanPress;
-    public RustedTimedPressureSwitchBlock(Properties pProperties,  int ticksToStayPressed, boolean arrowsCanPress) {
+
+    public RustedTimedPressureSwitchBlock(Properties pProperties, int ticksToStayPressed, boolean arrowsCanPress) {
         super(pProperties);
 
         this.ticksToStayPressed = ticksToStayPressed;
         this.arrowsCanPress = arrowsCanPress;
     }
+
     @Override
     public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
-        if (pPlayer.getAbilities().instabuild){
+        if (pPlayer.getAbilities().instabuild) {
             this.press(pState, pLevel, pPos);
             float f = pState.getValue(POWERED) ? 0.6F : 0.5F;
-            pLevel.playSound((Player)null, pPos, SoundEvents.STONE_BUTTON_CLICK_ON, SoundSource.BLOCKS, 1F, f);
+            pLevel.playSound((Player) null, pPos, SoundEvents.STONE_BUTTON_CLICK_ON, SoundSource.BLOCKS, 1F, f);
             pLevel.gameEvent(pPlayer, GameEvent.BLOCK_ACTIVATE, pPos);
         }
-        if (pHand == InteractionHand.MAIN_HAND && pPlayer.getMainHandItem().is(ModTags.Items.SHOVEL_ITEMS) && pState.getValue(POWERED) == true){
+        if (pHand == InteractionHand.MAIN_HAND && pPlayer.getMainHandItem().is(ModTags.Items.SHOVEL_ITEMS) && pState.getValue(POWERED) == true) {
             this.pull(pState, pLevel, pPos);
-            pLevel.playSound((Player)null, pPos, SoundEvents.STONE_BUTTON_CLICK_ON, SoundSource.BLOCKS, 1F, 1);
+            pLevel.playSound((Player) null, pPos, SoundEvents.STONE_BUTTON_CLICK_ON, SoundSource.BLOCKS, 1F, 1);
         }
-        if (pHand == InteractionHand.MAIN_HAND && pPlayer.getMainHandItem().is(ZeldaItems.MEGATON.get()) && pState.getValue(POWERED) == false){
+        if (pHand == InteractionHand.MAIN_HAND && pPlayer.getMainHandItem().is(ZeldaItems.MEGATON.get()) && pState.getValue(POWERED) == false) {
             this.press(pState, pLevel, pPos);
-            pLevel.playSound((Player)null, pPos, SoundEvents.STONE_PRESSURE_PLATE_CLICK_ON, SoundSource.BLOCKS, 1F, 1);
+            pLevel.playSound((Player) null, pPos, SoundEvents.STONE_PRESSURE_PLATE_CLICK_ON, SoundSource.BLOCKS, 1F, 1);
         }
         return InteractionResult.SUCCESS;
     }
-    public void entityInside(BlockState pState, Level pLevel, BlockPos pPos, Entity pEntity) {
-        LivingEntity livingentity = (LivingEntity) pEntity;
 
-        if (!pLevel.isClientSide && pState.getValue(POWERED).equals(false))  {
-            if ( livingentity.getItemBySlot(EquipmentSlot.FEET).is(ZeldaItems.HEAVY_BOOTS.get()) || livingentity.getItemBySlot(EquipmentSlot.HEAD).is(ZeldaItems.GORON_MASK.get()) ){
-                this.press(pState,pLevel,pPos);
-                pLevel.playSound((Player)null, pPos, SoundEvents.STONE_PRESSURE_PLATE_CLICK_ON, SoundSource.BLOCKS, 1F, 1);
+    public void entityInside(BlockState pState, Level pLevel, BlockPos pPos, Entity pEntity) {
+        if (pEntity instanceof LivingEntity livingentity) {
+            if (livingentity.getItemBySlot(EquipmentSlot.FEET).is(ZeldaItems.HEAVY_BOOTS.get()) || livingentity.getItemBySlot(EquipmentSlot.HEAD).is(ZeldaItems.GORON_MASK.get())) {
+                this.press(pState, pLevel, pPos);
+                pLevel.playSound((Player) null, pPos, SoundEvents.STONE_PRESSURE_PLATE_CLICK_ON, SoundSource.BLOCKS, 1F, 1);
             }
-        }}
+        }
+    }
 
     private void updateNeighbours(BlockState pState, Level pLevel, BlockPos pPos) {
         pLevel.updateNeighborsAt(pPos, this);
@@ -81,50 +83,53 @@ public class RustedTimedPressureSwitchBlock extends Block {
         pLevel.updateNeighborsAt(pPos.south(), this);
         pLevel.updateNeighborsAt(pPos.north(), this);
     }
+
     public void press(BlockState pState, Level pLevel, BlockPos pPos) {
         pLevel.setBlock(pPos, pState.setValue(POWERED, Boolean.valueOf(true)), 3);
         this.updateNeighbours(pState, pLevel, pPos);
         pLevel.scheduleTick(pPos, this, this.ticksToStayPressed);
     }
+
     public void pull(BlockState pState, Level pLevel, BlockPos pPos) {
-        pState = pState.setValue(POWERED,false);
+        pState = pState.setValue(POWERED, false);
         pLevel.setBlockAndUpdate(pPos, pState);
         this.updateNeighbours(pState, pLevel, pPos);
     }
 
     @Override
     public BlockState getStateForPlacement(BlockPlaceContext pContext) {
-        return this.defaultBlockState().setValue(POWERED,false);
+        return this.defaultBlockState().setValue(POWERED, false);
     }
 
     @Override
     public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
         Vec3 vec3 = pState.getOffset(pLevel, pPos);
-        if (pState.getValue(POWERED)){
+        if (pState.getValue(POWERED)) {
             return FLAT.move(vec3.x, vec3.y, vec3.z);
-        }
-        else
-            return SHAPE2.move(vec3.x, vec3.y, vec3.z);
+        } else return SHAPE2.move(vec3.x, vec3.y, vec3.z);
     }
+
     @Override
     public void onRemove(BlockState pState, Level pLevel, BlockPos pPos, BlockState pNewState, boolean pIsMoving) {
         if (!pIsMoving && !pState.is(pNewState.getBlock())) {
             if (pState.getValue(POWERED)) {
                 this.updateNeighbours(pState, pLevel, pPos);
             }
-            for(Direction direction : Direction.values()) {
+            for (Direction direction : Direction.values()) {
                 pLevel.updateNeighborsAt(pPos.relative(direction), this);
             }
             super.onRemove(pState, pLevel, pPos, pNewState, pIsMoving);
         }
     }
+
     public void tick(BlockState pState, ServerLevel pLevel, BlockPos pPos, RandomSource pRandom) {
         if (pState.getValue(POWERED)) {
             this.checkPressed(pState, pLevel, pPos);
         }
     }
+
     protected void checkPressed(BlockState pState, Level pLevel, BlockPos pPos) {
-        AbstractArrow abstractarrow = this.arrowsCanPress ? pLevel.getEntitiesOfClass(AbstractArrow.class, pState.getShape(pLevel, pPos).bounds().move(pPos)).stream().findFirst().orElse((AbstractArrow)null) : null;
+        AbstractArrow abstractarrow = this.arrowsCanPress ? pLevel.getEntitiesOfClass(AbstractArrow.class, pState.getShape(pLevel, pPos).bounds().move(pPos)).stream().findFirst().orElse((AbstractArrow) null) : null;
         boolean flag = abstractarrow != null;
         boolean flag1 = pState.getValue(POWERED);
         if (flag != flag1) {
@@ -136,28 +141,34 @@ public class RustedTimedPressureSwitchBlock extends Block {
             pLevel.scheduleTick(new BlockPos(pPos), this, this.ticksToStayPressed);
         }
     }
-    public static VoxelShape full(){
+
+    public static VoxelShape full() {
         VoxelShape shape = Shapes.empty();
         shape = Shapes.join(shape, Shapes.box(0.125, 0, 0.125, 0.875, 0.125, 0.875), BooleanOp.OR);
         return shape;
     }
-    public static VoxelShape flat(){
+
+    public static VoxelShape flat() {
         VoxelShape shape = Shapes.empty();
         shape = Shapes.join(shape, Shapes.box(0.125, 0, 0.125, 0.875, 0.03125, 0.875), BooleanOp.OR);
         return shape;
     }
+
     @Override
     public boolean isSignalSource(BlockState pState) {
         return true;
     }
+
     @Override
     public int getSignal(BlockState pBlockState, BlockGetter pBlockAccess, BlockPos pPos, Direction pSide) {
         return pBlockState.getValue(POWERED) && Direction.UP != pSide ? 15 : 0;
     }
+
     @Override
     public int getDirectSignal(BlockState pBlockState, BlockGetter pBlockAccess, BlockPos pPos, Direction pSide) {
         return pBlockState.getValue(POWERED) ? 15 : 0;
     }
+
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
         pBuilder.add(POWERED);
