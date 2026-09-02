c575e436c205322ac36ceeea66d1efd86af44f63
Kamtheman56 <misterman3@outlook.com>
2026-09-01 22:31:15 -0500

    Another minor hotfix! - True Master Sword has been updated to be able to Spin Attack. Mistakenly this was overriding the Use function.

--- src/main/java/com/kamth/zeldamod/item/items/weapons/swords/master/TrueMasterSwordItem.java
+++ src/main/java/com/kamth/zeldamod/item/items/weapons/swords/master/TrueMasterSwordItem.java
@@ -21,7 +21,7 @@ import net.minecraft.world.level.Level;
 import javax.annotation.Nullable;
 import java.util.List;
 
-public class TrueMasterSwordItem extends GloomBreakingSword implements IBeamShootAction {
+public class TrueMasterSwordItem extends GloomBreakingSword {
 
     public TrueMasterSwordItem(Tier pTier, int pAttackDamageModifier, float pAttackSpeedModifier, Properties pProperties, int gloomDestroySpeed) {
         super(pTier, pAttackDamageModifier, pAttackSpeedModifier, pProperties, gloomDestroySpeed);
@@ -30,11 +30,7 @@ public class TrueMasterSwordItem extends GloomBreakingSword implements IBeamShoo
     public TrueMasterSwordItem(Tier pTier, int pAttackDamageModifier, float pAttackSpeedModifier, Properties pProperties) {
         this(pTier, pAttackDamageModifier, pAttackSpeedModifier, pProperties, 28);
     }
-
-    @Override
-    public boolean healthRequirement() {
-        return true;
-    }
+    
 
     @Override
     public int swingCooldownDuration() {
@@ -55,27 +51,6 @@ public class TrueMasterSwordItem extends GloomBreakingSword implements IBeamShoo
             components.add(Component.translatable("item.zeldamod.master_sword_true.description_basic_2").withStyle(ChatFormatting.GOLD).withStyle(ChatFormatting.ITALIC));
         }
     }
-
-    //Only enabled if Alternative Sword Beams are allowed
-    @Override
-    public InteractionResultHolder<ItemStack> use(Level world, Player player, InteractionHand hand) {
-        ItemStack itemstack = player.getItemInHand(hand);
-
-        if (player.isCrouching() && Config.alternative_sword_beams() && !Config.sword_beams() && !player.getCooldowns().isOnCooldown(this)){
-            player.getCooldowns().addCooldown((Item) this, swingCooldownDuration());
-            world.playSound(null, player.getX(), player.getY(), player.getZ(), SoundEvents.PLAYER_ATTACK_SWEEP, SoundSource.PLAYERS, 0.8f, 5 / (world.getRandom().nextFloat() * 0.4f + 0.8f));
-            SwordBeam projectile = new SwordBeam(world, player);
-            projectile.setOwner(player);
-            projectile.setPos(player.getEyePosition().add(0, -0.1, 0));
-            projectile.shootFromRotation(player, player.xRotO, player.yRotO, 0, 1.6f, 0);
-            world.addFreshEntity(projectile);
-
-            return InteractionResultHolder.consume(itemstack);
-        }
-        return InteractionResultHolder.pass(itemstack);
-    }
-
-
 }
 
 
